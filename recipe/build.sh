618890e34372eac5196ad9ac50e2c1bfed735650
Ben Moss <ben@mossity.com>
2025-03-04 17:01:28 -0500

    Start over with rattler-build

---
+++ recipe/build.sh
@@ -0,0 +1,10 @@
+cmake --preset 'CPU' \
+    && cmake --build --parallel --preset 'CPU' \
+    && cmake --install build --component CPU --strip --parallel 8
+
+
+export GOFLAGS="'-ldflags=-s -w -X=github.com/ollama/ollama/version.Version=${PKG_VERSION} -X=github.com/ollama/ollama/server.mode=release'"
+export CGO_ENABLED=1
+go build -trimpath -buildmode=pie -o $PREFIX/bin/ollama .
+
+go-licenses save . --save_path="$SRC_DIR/license-files/" 
