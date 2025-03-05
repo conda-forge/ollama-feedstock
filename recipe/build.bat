cb24f69ffe5e514fcd94845028b14c903fd00fb9
Ben Moss <ben@mossity.com>
2025-03-05 11:47:50 -0500

    try for windows

---
+++ recipe/build.bat
@@ -0,0 +1,9 @@
+cmake --preset CPU || exit 1
+cmake --build --parallel --preset CPU || exit 1
+cmake --install build --component CPU --strip --parallel 8 || exit 1
+
+
+set CGO_ENABLED=1
+go build -ldflags "-s -w -X=github.com/ollama/ollama/version.Version=${PKG_VERSION} -X=github.com/ollama/ollama/server.mode=release" -trimpath -buildmode=pie -o %LIBRARY_BIN%\ollama.exe . || exit 1
+
+go-licenses save . --save_path="%SRC_DIR%/license-files/" || exit 1
