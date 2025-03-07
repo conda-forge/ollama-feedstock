c6592c540328ca88d205e45df05a5e483379afb2
Ben Moss <ben@mossity.com>
2025-03-07 13:33:17 -0500

    try some cuda

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,13 +3,27 @@ then
     export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 fi
 
-cmake --preset 'CPU' \
-    && cmake --build --parallel --preset 'CPU' \
-    && cmake --install build --component CPU --strip --parallel 8
+if [[ ${cuda_compiler_version} != "None" ]]; then
+  if [[ ${cuda_compiler_version} == 11.8 ]]; then
+    cmake --preset 'CUDA 11' \
+        && cmake --build --parallel --preset 'CUDA 11' \
+        && cmake --install build --component CUDA --strip --parallel 8
+  elif [[ ${cuda_compiler_version} == 12.0 || ${cuda_compiler_version} == 12.6 ]]; then
+    cmake --preset 'CUDA 12' \
+        && cmake --build --parallel --preset 'CUDA 12' \
+        && cmake --install build --component CUDA --strip --parallel 8
+  else
+    echo "unsupported cuda version"
+    exit 1
+  fi
+else
+    cmake --preset 'CPU' \
+        && cmake --build --parallel --preset 'CPU' \
+        && cmake --install build --component CPU --strip --parallel 8
+fi
 
 
-export GOFLAGS="'-ldflags=-s -w -X=github.com/ollama/ollama/version.Version=${PKG_VERSION} -X=github.com/ollama/ollama/server.mode=release'"
 export CGO_ENABLED=1
-go build -trimpath -buildmode=pie -o $PREFIX/bin/ollama .
+go build -trimpath -buildmode=pie -ldflags='-s -w -X=github.com/ollama/ollama/version.Version=${PKG_VERSION} -X=github.com/ollama/ollama/server.mode=release'-o $PREFIX/bin/ollama .
 
 go-licenses save . --save_path="$SRC_DIR/license-files/" 
