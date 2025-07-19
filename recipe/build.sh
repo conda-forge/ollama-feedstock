a8394bb231eb6d506bab69dac206319abd1edb35
Ben Moss <ben@mossity.com>
2025-07-19 16:55:04 -0400

    Fix cuda version matching to support 12.9

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,25 +5,27 @@ if [[ "$target_platform" == osx-* ]]; then
     export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 fi
 
-if [[ ${cuda_compiler_version} != "None" ]]; then
-  if [[ ${cuda_compiler_version} == 11.8 ]]; then
-    cmake ${CMAKE_ARGS} --preset 'CUDA 11' \
-        && cmake --build --preset 'CUDA 11' \
-        && cmake --install build --component CUDA --strip
-  elif [[ ${cuda_compiler_version} == 12.0 || ${cuda_compiler_version} == 12.6 ]]; then
-    cmake ${CMAKE_ARGS} --preset 'CUDA 12' \
-        && cmake --build --preset 'CUDA 12' \
-        && cmake --install build --component CUDA --strip
-  else
-    echo "unsupported cuda version"
-    exit 1
-  fi
-else
+if [[ -z "$cuda_compiler_version" ]]; then
     cmake ${CMAKE_ARGS} --preset 'CPU' \
         && cmake --build --preset 'CPU' \
         && cmake --install build --component CPU --strip
+else
+  case "$cuda_compiler_version" in
+    11.*)
+      cmake ${CMAKE_ARGS} --preset 'CUDA 11' \
+          && cmake --build --preset 'CUDA 11' \
+          && cmake --install build --component CUDA --strip
+          ;;
+    12.*)
+      cmake ${CMAKE_ARGS} --preset 'CUDA 12' \
+          && cmake --build --preset 'CUDA 12' \
+          && cmake --install build --component CUDA --strip
+          ;;
+    *)
+      echo "unsupported cuda version"
+      exit 1
+  esac
 fi
-
 
 go build -trimpath -buildmode=pie -ldflags="-s -w -X=github.com/ollama/ollama/version.Version=${PKG_VERSION} -X=github.com/ollama/ollama/server.mode=release" -o $PREFIX/bin/ollama .
 
