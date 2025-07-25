ff303ce7e4a8969d3cd0cf0109e98683e9f9886a
Ben Moss <ben@mossity.com>
2025-07-24 22:04:22 -0400

    reduce parallelism, refactor

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 set -ex
 
+build_and_install() {
+    local preset="$1"
+    local component="$2"
+    cmake ${CMAKE_ARGS:+$CMAKE_ARGS} --preset "$preset" \
+        && cmake --build --preset "$preset" \
+        && cmake --install build --component "$component" --strip
+}
+
 if [[ "$target_platform" == osx-* ]]; then
     export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 fi
+export CMAKE_BUILD_PARALLEL_LEVEL=1
 
 if [[ -z "$cuda_compiler_version" || "$cuda_compiler_version" == "None" ]]; then
-    cmake ${CMAKE_ARGS} --preset 'CPU' \
-        && cmake --build --preset 'CPU' \
-        && cmake --install build --component CPU --strip
+    build_and_install "CPU" "CPU"
 else
   case "$cuda_compiler_version" in
-    11.*)
-      cmake ${CMAKE_ARGS} --preset 'CUDA 11' \
-          && cmake --build --preset 'CUDA 11' \
-          && cmake --install build --component CUDA --strip
-          ;;
-    12.*)
-      cmake ${CMAKE_ARGS} --preset 'CUDA 12' \
-          && cmake --build --preset 'CUDA 12' \
-          && cmake --install build --component CUDA --strip
-          ;;
+    11.*) build_and_install "CUDA 11" "CUDA" ;;
+    12.*) build_and_install "CUDA 12" "CUDA" ;;
     *)
       echo "unsupported cuda version"
       exit 1
